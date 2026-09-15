49a733eeca0aade350fc7a368431dcadf6d7d4be
Keysa404 <suwarnoida02@gmail.com>
2026-09-15 14:37:57 +0700

    Add files via upload

--- PemilihanBilangan14.java
+++ PemilihanBilangan14.java
@@ -8,11 +8,8 @@ public class PemilihanBilangan14 {
         System.out.print("Masukkan sebuah angka ");
         int angka = key.nextInt();
 
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String hasil = (angka % 2 == 0) ? "termasuk bilangan genap" : "termasuk bilangan ganjil";
+        System.out.println("Angka " + angka + " " + hasil);
 
         key.close();
     }
